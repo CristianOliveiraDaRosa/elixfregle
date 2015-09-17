fe45c14394f195eb78d1c1c186befa15f250bb98
Cristian Oliveira da Rosa <contato@cristianoliveira.com.br>
2015-09-16 23:43:22 -0300

    Adding Http client dependency

--- mix.exs
+++ mix.exs
@@ -25,6 +25,9 @@ defmodule Elixfregle.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [
+      {:ibrowse, github: "cmullaparthi/ibrowse", tag: "v4.1.2"},
+      {:httpotion, "~> 2.1.0"}
+    ]
   end
 end
